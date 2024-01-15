84ccab29264b970980d7516755294d7690d80035
Hieu Hoang <dichvuaz88.info@gmail.com>
2024-01-15 16:13:37 +0700

    complete sign up page functionality

--- client/src/components/Input/Input.jsx
+++ client/src/components/Input/Input.jsx
@@ -1,10 +1,12 @@
-export default function Input({ type, placeholder }) {
+export default function Input({ id, type, placeholder, onChange }) {
   return (
     <div className="">
       <input
+        id={id}
         type={type}
         placeholder={placeholder}
         required
+        onChange={onChange}
         className="bg-slate-100 p-3 rounded-lg w-full"
       />
     </div>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,17 +1,67 @@
 import { Link } from "react-router-dom";
 import Input from "../components/Input/Input";
+import { useState } from "react";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
+
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      setLoading(false);
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form className="flex flex-col gap-4">
-        <Input type="text" placeholder="Username" />
-        <Input type="email" placeholder="Email" />
-        <Input type="password" placeholder="Password" />
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <Input
+          id="username"
+          type="text"
+          placeholder="Username"
+          onChange={handleChange}
+        />
+        <Input
+          id="email"
+          type="email"
+          placeholder="Email"
+          onChange={handleChange}
+        />
+        <Input
+          id="password"
+          type="password"
+          placeholder="Password"
+          onChange={handleChange}
+        />
 
-        <button className="p-3 bg-slate-700 text-white font-semibold rounded-lg my-3 uppercase hover:opacity-95 disabled:opacity-80">
-          Sign up
+        <button
+          disabled={loading}
+          className="p-3 bg-slate-700 text-white font-semibold rounded-lg my-3 uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "...Loading" : "Sign up"}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
@@ -20,6 +70,9 @@ export default function SignUp() {
           <span className="text-blue-500 font-medium">Sign in</span>
         </Link>
       </div>
+      <p className="text-red-700 text-center font-bold mt-4">
+        {error && "Username && Email could exist"}
+      </p>
     </div>
   );
 }

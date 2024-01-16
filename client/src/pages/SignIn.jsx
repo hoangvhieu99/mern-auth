066ac6c043d3df9a817295ade1b5c6cd043398b1
Hieu Hoang <dichvuaz88.info@gmail.com>
2024-01-16 15:15:45 +0700

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,20 +1,25 @@
 import { Link, useNavigate } from "react-router-dom";
 import Input from "../components/Input/Input";
 import { useState } from "react";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
-      setError(false);
+      dispatch(signInStart());
 
       const res = await fetch("/api/auth/signin", {
         method: "POST",
@@ -24,20 +29,20 @@ export default function SignIn() {
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-      setLoading(false);
       if (data.success === false) {
-        setError(true);
+        dispatch(signInFailure(data));
         return;
       }
+      dispatch(signInSuccess(data));
       navigate("/");
     } catch (error) {
-      setLoading(false);
-      setError(true);
+      console.log(error);
+      dispatch(signInFailure(error));
     }
   };
   return (
     <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <Input
           id="email"
@@ -56,7 +61,7 @@ export default function SignIn() {
           disabled={loading}
           className="p-3 bg-slate-700 text-white font-semibold rounded-lg my-3 uppercase hover:opacity-95 disabled:opacity-80"
         >
-          {loading ? "...Loading" : "Sign ip"}
+          {loading ? "...Loading" : "Sign in"}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
@@ -66,7 +71,7 @@ export default function SignIn() {
         </Link>
       </div>
       <p className="text-red-700 text-center font-bold mt-4">
-        {error && "Username && Email could exist"}
+        {error ? error || "something went wrong" : ""}
       </p>
     </div>
   );

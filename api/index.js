1e69f29592864c3e0982685aa01a740e5b7dfa92
Hieu Hoang <dichvuaz88.info@gmail.com>
2024-01-24 16:24:48 +0700

    create update user api route

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import useRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
-
+import cookieParser from "cookie-parser";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -14,7 +14,11 @@ mongoose
     console.log(err);
   });
 const app = express();
+
 app.use(express.json());
+
+app.use(cookieParser());
+
 app.listen(3000, () => {
   console.log("Server listening on port 3000!");
 });
